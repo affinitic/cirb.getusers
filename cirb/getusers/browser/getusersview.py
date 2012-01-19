b0f11793af93d8352e3c672d8bba5ff32905b2d5
Benoît Suttor <bsuttor@cirb.irisnet.be>
2012-01-19 11:47:48 +0100

    Only excel export

--- cirb/getusers/browser/getusersview.py
+++ cirb/getusers/browser/getusersview.py
@@ -16,15 +16,16 @@ class IGetUsersForm(Interface):
     """
     Get Users view interface
     """
-    roles = Choice(title=u'Roles', vocabulary='Available Roles', required=False)
-    output = Choice(title=u'Output', vocabulary='Available Outputs', required=True)
+    roles = Choice(title=u'Roles', vocabulary='Available Roles', required=True, default="True")
+    output = Choice(title=u'Output', vocabulary='Available Outputs', required=True, default="Excel")
     
 def availableRoles(context):
     subjects = (_(u'True'), _(u'False'), )
     return SimpleVocabulary.fromValues(subjects)
 
 def availableOutputs(context):
-    subjects = (_(u'Excel'), _(u'Pdf'), )
+    #subjects = (_(u'Excel'), _(u'Pdf'), )
+    subjects = (_(u'Excel'),)
     return SimpleVocabulary.fromValues(subjects)
 
 
@@ -34,7 +35,11 @@ class Users(list):
     """
     def __init__(self, membership, roles=False):
         self.membership = membership
-        self.roles = roles
+        if roles == "False" or roles == "Non" or roles == "Nee":
+            self.roles = False
+        else:
+            self.roles = True
+        self.plone_roles = [(0,'Authenticated'), (1,'Site Administrator'), (2,'Member'), (3,'Manager'), (4,'Editor'), (5,'Reader'), (6,'Contributor'), (7,'Reviewer')]
         self.update_users()
     
     def update_users(self):
@@ -44,21 +49,35 @@ class Users(list):
             item['name'] = member.getUserName()
             if self.roles:
                 item['roles'] = member.getRoles()
-            self.append(item)       
+            self.append(item)
     
     def get_users(self):
         return self
 
     def get_excel_users(self):
-        users = "%s;%s;%s;%s;%s;" % (_(u'Name'),_(u'Site Administrator'),_(u'Reviewer'),_(u'Authenticated'),_(u'Member'))
+        users = "%s\n" % self.get_excel_first_line()
         for user in self:
             users += "%s;" % user.get('name')
-            roles = user.get('roles')
-            for role in roles:
-                users += "%s;" % role
+            if self.roles:
+                xls_roles = ["" for x in range(len(self.plone_roles))]
+                for role in user.get('roles'):
+                    for k, v in self.plone_roles:
+                        if role == v:
+                            xls_roles[k] = "X"
+                    
+                users += ";".join(xls_roles)
             users += "\n"
         return users
     
+    def get_excel_first_line(self):
+        #TODO get translation
+        results = ['Name']
+        if self.roles:
+            for k, v in sorted(self.plone_roles):
+                results.append(v)
+        return ";".join(results)
+            
+
 
 class GetUsersForm(PageForm):
     label = _('Get user list')
@@ -69,10 +88,11 @@ class GetUsersForm(PageForm):
     @form.action("send")
     def action_send(self, action, data):
         roles = data.get('roles', False)
+        membership = getToolByName(self, "portal_membership")
         RESPONSE = self.request.response
-        users = Users(getToolByName(self, 'portal_membership'), roles)
+        users = Users(membership, roles)
         if data.get('output').lower() == "excel":
-	    RESPONSE.setHeader("Content-type","application/ms-excel")
+            RESPONSE.setHeader("Content-type","application/ms-excel")
             RESPONSE.setHeader("Content-disposition","attachment;filename=Users.xls")
 
         return users.get_excel_users()
--- cirb/getusers/tests/test_users.py
+++ cirb/getusers/tests/test_users.py
@@ -1,11 +1,14 @@
 # -*- coding: UTF-8 -*-
 import unittest2 as unittest
-import cirb.getusers.browser.getusersview import Users
+from cirb.getusers.browser.getusersview import Users
 
 class TestUsers(unittest.TestCase):
     def test_dummy(self):
         self.assertTrue(True)
 
-    def test_users_list(self):
+    def test_excel_first_line(self):
+        #membership = getToolByName(self, "portal_membership")
+        u = Users()
+        self.assertEqual(u.get_excel_first_line, '', 'Excel first line not correct')
         #add users
-        Users("membership")
+        
