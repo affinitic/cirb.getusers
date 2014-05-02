bc8178f1bcef278f58596ab90bfbf3819b05ce0c
Godefroid Chapelle <gotcha@bubblenet.be>
2014-05-02 09:20:20 +0200

    pyflakes pep8

--- cirb/getusers/tests/test_users.py
+++ cirb/getusers/tests/test_users.py
@@ -3,21 +3,32 @@ import unittest2 as unittest
 from cirb.getusers.browser.getusersview import Users
 from cirb.getusers.testing import GETUSERS_INTEGRATION
 
-from plone.app.testing.interfaces import TEST_USER_ID, TEST_USER_NAME, TEST_USER_PASSWORD, TEST_USER_ROLES
+from plone.app.testing.interfaces import TEST_USER_PASSWORD, TEST_USER_ROLES
 from Products.CMFCore.utils import getToolByName
 
 import csv
 from StringIO import StringIO
+
+
 class TestUsers(unittest.TestCase):
     layer = GETUSERS_INTEGRATION
 
     def test_csv_return(self):
         portal = self.layer["portal"]
         pas = portal['acl_users']
-        pas.source_users.addUser("dummy_userid", "dummy_username", TEST_USER_PASSWORD, )
-        membership = getToolByName(portal, "portal_membership") 
+        pas.source_users.addUser(
+            "dummy_userid",
+            "dummy_username",
+            TEST_USER_PASSWORD,
+        )
+        membership = getToolByName(portal, "portal_membership")
         member = membership.getMemberById('dummy_userid')
-        member.setMemberProperties({'fullname':'dummy', "email":"dummy@user.id"})
+        member.setMemberProperties(
+            {
+                'fullname': 'dummy',
+                "email": "dummy@user.id"
+            }
+        )
         u = Users(membership, False)
         lines = u.get_csv_users().splitlines()
         self.assertEqual(lines[0], "Id;Name;Email")
@@ -26,16 +37,26 @@ class TestUsers(unittest.TestCase):
     def test_cross(self):
         portal = self.layer["portal"]
         pas = portal['acl_users']
-        pas.source_users.addUser("dummy_userid", "dummy_username", TEST_USER_PASSWORD, )
+        pas.source_users.addUser(
+            "dummy_userid",
+            "dummy_username",
+            TEST_USER_PASSWORD,
+        )
         for role in TEST_USER_ROLES:
-            pas.portal_role_manager.doAssignRoleToPrincipal("dummy_userid", role)
-        membership = getToolByName(portal, "portal_membership") 
+            pas.portal_role_manager.doAssignRoleToPrincipal(
+                "dummy_userid", role
+            )
+        membership = getToolByName(portal, "portal_membership")
         member = membership.getMemberById('dummy_userid')
-        member.setMemberProperties({'fullname':'dummy', "email":"dummy@user.id"})
+        member.setMemberProperties(
+            {
+                'fullname': 'dummy',
+                "email": "dummy@user.id"
+            }
+        )
         u = Users(membership, True)
         dr = csv.DictReader(StringIO(u.get_csv_users()), delimiter=";")
         users_list = list(dr)
-        self.assertEqual(len(users_list),2)
+        self.assertEqual(len(users_list), 2)
         self.assertEqual(users_list[0].get('Member'), "X")
         self.assertNotEqual(users_list[1].get('Manager'), "X")
-
